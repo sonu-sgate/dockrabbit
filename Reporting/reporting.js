e094e43c50cc8acda17414051c2fa34ea126f0a8
sonu-sgate <sgate.sonu@gmail.com>
2024-01-10 13:06:47 +0530

    changes

--- Reporting/reporting.js
+++ Reporting/reporting.js
@@ -3,7 +3,9 @@ const amqp=require('amqplib')
 async function startReportingService(){
     const connection=await amqp.connect('amqp://127.0.0.1')
     const channel=await connection.createChannel()
-
+    const result=await channel.get('Invoicing_queue')
+// console.log((JSON.parse(result.content.toString())))
+console.log(result,"result")
     await channel.assertQueue('reporting_queue',{durable:false})
 console.log('Reporting service is wating for reporting requests')
 
@@ -11,7 +13,14 @@ channel.consume('reporting_queue',(msg)=>{
 const reportingRequest=JSON.parse(msg.content.toString())
 console.log(`Recieved reporing request:${JSON.stringify(reportingRequest)}`)
 
-},{noAck:true})
+// if(result){
+//     console.log(JSON.parse(result.content.toString()))
+// }else{
+//     console.log("hello")
+// }
+
+channel.ack(msg)
+},{noAck:false})
 
 }
 startReportingService()
